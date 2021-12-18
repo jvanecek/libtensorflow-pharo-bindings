a0fa69bd49a058066a486b94f15f205b34eb5f06
jvanecek <juann.vanecek@gmail.com>
2021-12-18 13:52:07 -0500

    :sparkles: upgrade to TF 2.7.0
    Refactor TF String model

--- scripts/install-tensorflow.sh
+++ scripts/install-tensorflow.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$VERSION" ]
 then
-	VERSION=${1:-2.3.1}
+	VERSION=${1:-2.7.0}
 fi
 
 curl -0 "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-${VERSION}.tar.gz" >> libtensorflow.tar.gz
